965435c147af68a343a2fccc2dd14d0227f6ff3c
Mooneer Salem <mooneer@gmail.com>
2026-07-23 20:09:42 -0700

    Oops, forgot division.

--- src/pipeline/RADEReceiveStep.cpp
+++ src/pipeline/RADEReceiveStep.cpp
@@ -199,7 +199,7 @@ short* RADEReceiveStep::execute(short* inputSamples, int numInputSamples, int* n
         // demod per frame processing
         for(int i=0; i<nin; i++)
         {
-            inputBufCplx_[i].real = inputBuf_[i] * RADE_INT16_SCALE;
+            inputBufCplx_[i].real = inputBuf_[i] / RADE_INT16_SCALE;
             inputBufCplx_[i].imag = 0.0;
         }
 
