7873018d4d487acf87407fc2dc0bcdf5ee29eaa6
Mooneer Salem <mooneer@gmail.com>
2026-07-29 00:19:37 -0700

    Revert "Use version of RADE that has warnings silenced. (testing)"
    This reverts commit b2822639d9248ff528289bbfc0aa154eab782bb0.

--- cmake/BuildRADE.cmake
+++ cmake/BuildRADE.cmake
@@ -9,7 +9,7 @@ ExternalProject_Add(build_rade
    SOURCE_DIR rade_src
    BINARY_DIR rade_build
    GIT_REPOSITORY https://github.com/freedv/rade_c
-   GIT_TAG ms-silence-build-warnings
+   GIT_TAG main
    GIT_SUBMODULES ""
    GIT_SUBMODULES_RECURSE NO
    CMAKE_ARGS ${RADE_CMAKE_ARGS}
